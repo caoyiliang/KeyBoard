6df335b8b38e3d470381725a22bb76213ae1156c
曹一梁 <caoyiliang@me.com>
2023-09-28 15:29:45 +0800

    键盘工具

---
+++ Qwerty/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Qwerty
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            KeyBoard.KeyBoard keyBoard = new KeyBoard.KeyBoard();
+            keyBoard.Show();
+        }
+    }
+}
